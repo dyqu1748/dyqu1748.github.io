44237065fbce54d23e557e2ef39d0fe426c2e1c6
dyqu1748 <47007591+dyqu1748@users.noreply.github.com>
2020-09-01 13:03:59 -0600

    Add files via upload

---
+++ phone.js
@@ -0,0 +1,38 @@
+$(document).ready(function() { // do this when the document is loaded
+	$("#content_dial").show(); // show the element with ID "element"
+	$("#content_contact").hide();	// hide other element
+    $("#content_addCont").hide();
+    $("#dialer").css("background-color","white");
+    $("#content_dial").css("background-color","white");
+});
+
+$("#dialer").click(function() { // when "button_id" is clicked
+	$("#content_dial").show(); // show element
+    $("#content_contact").hide();	// hide other element
+    $("#content_addCont").hide();
+    $("#dialer").css("background-color","white");
+    $("#addCont").css("background-color","lightgrey");
+    $("#contact").css("background-color","lightgrey");
+});
+
+$("#contact").click(function() { // when "button_id" is clicked
+	$("#content_contact").show(); // show element
+    $("#content_dial").hide();	// hide other element
+    $("#content_addCont").hide();
+    $("#contact").css("background-color","white");
+    $("#dialer").css("background-color","lightgrey");
+    $("#addCont").css("background-color","lightgrey");
+});
+
+$("#addCont").click(function() { // when "button_id" is clicked
+	$("#content_addCont").show(); // show element
+    $("#content_contact").hide();	// hide other element
+    $("#content_dial").hide();
+    $("#addCont").css("background-color","white");
+    $("#dialer").css("background-color","lightgrey");
+    $("#contact").css("background-color","lightgrey");
+});
+
+function show(num){
+    document.getElementById("disp").value+=num
+}
